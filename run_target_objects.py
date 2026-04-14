7ad15573a86f2c5bde428d4494f8cdc8c6f064b5
JiangBowen0008 <jbw0008@outlook.com>
2026-04-14 17:34:44 -0500

    Test SA variant on all objects

--- run_target_objects.py
+++ run_target_objects.py
@@ -77,7 +77,7 @@ def run_object(name):
             steps=300, lr=0.005,
             save_path=save_file,
             opt_sections="ABCD",
-            opt_variant="CA",
+            opt_variant="SA",
         )
         elapsed = time.time() - t0
 
